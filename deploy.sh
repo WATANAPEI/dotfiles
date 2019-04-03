3b4f7b75d864f018f90605ece55ddd67c5c58c23
WATANAPEI <pei2791150@gmail.com>
2019-04-03 18:25:29 +0200

    modify deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,35 @@
-#! /bin/bash
+#! /bin/sh
 
+DOTFILES_BASENAME="dotfiles"
+DOT="${DOT:-$HOME/$DOTFILES_BASENAME}"
+BACKUP_DIR="$HOME/dotfiles_backup"
+TIME=$(date +%Y%m%d_%H%M%S)
 echo "----DOT ONLY---"
-for f in .??* 
-do echo ${f}; 
+for f in .??*
+do echo ${f};
 done
+
+cd "$DOT"
+
+echo "start setup..."
+link(){
+    #if the same file exists and is not symbolic link
+    if [ -e "$HOME/$1" -a \! -L "$HOME/$1" ]; then
+        # make backup by mv
+        $EVAL mkdir -p $BACKUP_DIR
+        echo "Backup: $1"
+        $EVAL mv -v "$HOME/$1" "$BACKUP_DIR/$1.$TIME"
+    fi
+    if [ -L $HOME/$1 ]; then
+        $EVAL rm $HOME/$1
+    fi
+    #make symbolic link
+    $EVAL ln -snfv $DOT/$1 $HOME/$1
+}
+
+link .vim
+link .vimrc
+link .zshrc
+
+echo "finish setup.."
 
